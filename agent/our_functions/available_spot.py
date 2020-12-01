3f612150041ec70a4326b90c39e4b55da32c9497
GWFrank <frank910311@gmail.com>
2020-12-01 15:26:36 +0800

    add makemove.py, update availa

--- agent/our_functions/available_spot.py
+++ agent/our_functions/available_spot.py
@@ -2,21 +2,26 @@ def getAvailableSpot(obs, color):
     """
     Parameters
     -----------
-    obs: dict 
-        board status
+    obs:    dict 
+            board status
 
-        key: int 0 ~ 63
-        value: [-1, 0 ,1]
-                -1 : black
-                0 : empty
-                1 : white
+            key: int 0 ~ 63
+            value: [-1, 0 ,1]
+                    -1 : black
+                    0 : empty
+                    1 : white
+    
+    color:  string
+            getting who's available spot
+
+            "black" or "white"
     
     Returns
     -----------
     list(tuple)
-    Each tuple represent cordinate of a available spot (x, y).
-    x : int 0 ~ 7
-    y : int 0 ~ 7
+            Each tuple represent cordinate of a available spot (x, y).
+            x : int 0 ~ 7
+            y : int 0 ~ 7
     """
     
     board = [obs[8*row:8*row+8] for row in range(8)]
@@ -34,6 +39,11 @@ def getAvailableSpot(obs, color):
     available_spot = []
 
     def chk_atk(horz, vert, row, col):
+        '''
+        horz : checking which direction on x
+        vert : checking which direction on y
+        row, col : attacking pivot's pos
+        '''
         nonlocal available_spot, allie, enemy, empty
         
         can_attack = False
