a428ae7e168f83b24ea8d03370cc5ff9b33f9002
GWFrank <frank910311@gmail.com>
2020-12-11 14:37:34 +0800

    agent testing tweaks

--- test_agents_singleprocess.py
+++ test_agents_singleprocess.py
@@ -2,9 +2,9 @@ from agent.GWFrank_func.match_agents import matchup
 from agent.GWFrank_func.test_agent_class import MinimaxTestAgent, LittleRandomTestAgent
 from agent.GWFrank_func.eval_funcs import posEval, posEvalEndgameVariation
 
-rounds = 5
+rounds = 10
 
-agents = [LittleRandomTestAgent(posEvalEndgameVariation, 5, 0.03),
+agents = [LittleRandomTestAgent(posEvalEndgameVariation, 5, 0.003),
           MinimaxTestAgent(posEvalEndgameVariation, 5)]
 agent_num = len(agents)
 
@@ -15,7 +15,7 @@ for a in range(agent_num):
         matchup(agents[a], agents[b], rounds)
 
 print("="*20)
-print(f"In {rounds*2*(agent_num)*(agent_num-1)} games...")
+print(f"In {rounds*(agent_num)*(agent_num-1)} games...")
 for a in agents:
     W, L, D = a.win, a.loss, a.draw
     rule, depth = a.rule, a.s_depth
